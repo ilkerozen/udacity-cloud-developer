179d94649a4ffbf64ec55122887c356fe70d2963
Ilker <ozen.ilker@gmail.com>
2020-06-17 01:18:40 +0300

    first commit

---
+++ udacity-serverless/client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '0utfyo03x9'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  
+  domain: 'dev-rm2ab2nj.us.auth0.com',            // Auth0 domain
+  clientId: 'AtywX3S5q3KRrhINgZOeLj1ndH8GK2P4',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
